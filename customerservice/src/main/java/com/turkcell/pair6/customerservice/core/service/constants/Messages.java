cbfea1a5531844ec0ef7718cfdb50357b9f386b9
Resul Dinc <dincresull@gmail.com>
2024-04-05 22:05:54 +0300

    Business rule added to address delete method[FR31]

--- customerservice/src/main/java/com/turkcell/pair6/customerservice/core/service/constants/Messages.java
+++ customerservice/src/main/java/com/turkcell/pair6/customerservice/core/service/constants/Messages.java
@@ -15,5 +15,6 @@ public class Messages {
 
         public static final String CUSTOMER_WITH_THAT_ID_NOT_EXIST="customer.withThatIdNotExist";
 
+        public static final String CUSTOMER_HAS_NOT_MORE_THAN_ONE_ADDRESS = "customer.hasNotMoreThanOneAddress";
     }
 }
