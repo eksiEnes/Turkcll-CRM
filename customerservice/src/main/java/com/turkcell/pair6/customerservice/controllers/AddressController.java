849983af39a940524b7cd538627b0c3e9d23abb6
Resul Dinc <dincresull@gmail.com>
2024-05-04 15:41:06 +0300

    core integration to invoiceservice

--- customerservice/src/main/java/com/turkcell/pair6/customerservice/controllers/AddressController.java
+++ customerservice/src/main/java/com/turkcell/pair6/customerservice/controllers/AddressController.java
@@ -43,7 +43,7 @@ public class AddressController {
         addressService.delete(id);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping
     public void update(@RequestBody @Valid UpdateAddressRequest updateAddressRequest) {
         addressService.update(updateAddressRequest);
     }
