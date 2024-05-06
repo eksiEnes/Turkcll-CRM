9429d0312fb03189bab915b80a5bb7a73e05bece
Resul Dinc <dincresull@gmail.com>
2024-05-06 18:34:21 +0300

    FeignClientConfig added to invoiceservices

--- invoiceservice/src/main/java/com/turkcell/pair6/invoiceservice/clients/CustomerServiceClient.java
+++ invoiceservice/src/main/java/com/turkcell/pair6/invoiceservice/clients/CustomerServiceClient.java
@@ -1,5 +1,6 @@
 package com.turkcell.pair6.invoiceservice.clients;
 
+import com.turkcell.core.configuration.feign.FeignClientConfiguration;
 import com.turkcell.pair6.invoiceservice.services.dtos.requests.UpdateAddressRequest;
 import com.turkcell.pair6.invoiceservice.services.dtos.requests.AddAddressRequest;
 import jakarta.validation.Valid;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "customerservice")
+@FeignClient(name = "customerservice", configuration = FeignClientConfiguration.class)
 public interface CustomerServiceClient {
 
     @PostMapping("/api/addresses")
